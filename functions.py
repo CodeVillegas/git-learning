38176e28827452ac03de6a1718ce3d51e197142c
CodeVillegas <villegasptm15@gmail.com>
2019-06-25 11:29:35 -0500

    ahora he creado una funcion nueva

---
+++ functions.py
@@ -0,0 +1,9 @@
+def hello(name="Bot"):
+     print('Hello '+ name)
+
+name = input('Put your name: ')
+hello()
+
+add = lambda numberOne, numberTwo: numberOne + numberTwo
+
+print(add(10, 30))
---
+++ module.py
@@ -0,0 +1,2 @@
+def saludar(hola=input("Insert"), casa=input("Insert")):
+     print("hola" + hola)
